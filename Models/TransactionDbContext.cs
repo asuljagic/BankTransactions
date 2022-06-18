b8f99e35a913cf5191c0212593f72e2fd238bbe6
Adnan Suljagic <asuljagic1@etf.unsa.ba>
2022-06-18 15:11:25 +0200

    Add project files.

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactions.Models
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {}
+
+        public DbSet<Transaction> Transactions{ get; set; }
+    }
+}
